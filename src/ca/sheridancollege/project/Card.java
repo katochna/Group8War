4109ad89743f6f5cc2d3e85d63ff24479044f92e
NumethH <157844887+NumethH@users.noreply.github.com>
2024-03-09 23:16:59 -0500

    Comment Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -10,7 +10,7 @@ package ca.sheridancollege.project;
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  *
  * @author dancye
- * @author Numeth Handunpurage
+ * @author Numeth Handunpurage March 2024
  */
 public abstract class Card {
     //default modifier for child classes
